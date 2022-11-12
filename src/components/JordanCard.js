76557d4b04197ae81e6e1ef59e421514f13b9bd0
Bill Nguyen <billfl93@gmail.com>
2022-11-12 00:54:23 -0500

    set state and render shoes

--- src/components/JordanCard.js
+++ src/components/JordanCard.js
@@ -1,1 +1,15 @@
 import React from "react";
+
+function JordanCard({name, image, date, designer}) {
+    return (
+        <li className="card">
+          <img src={image} alt={name} />
+          <h4>{name}</h4>
+          <p>Date Released: {date}</p>
+          <p>Designer: {designer}</p>
+        </li>
+      );
+    }
+
+
+export default JordanCard;
--- src/components/JordanList.js
+++ src/components/JordanList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import JordanCard from "./JordanCard";
 
 function JordanList({jordans}) {
     const renderJordans = jordans.map((jordan) => (
