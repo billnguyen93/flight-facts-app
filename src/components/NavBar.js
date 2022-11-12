8399b5fe09252f46a98446393ad633ee2103037c
Bill Nguyen <billfl93@gmail.com>
2022-11-11 22:46:57 -0500

    add background and navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function NavBar() {
+    return (
+        <div class="ui inverted menu">
+            <a class="active item">Home</a>
+            <a class="item">Favorites</a>
+            <a class="item">Add Jordan</a>
+        </div>
+    )
+}
+
+export default NavBar;
